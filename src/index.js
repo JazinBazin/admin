2fb1f44c8eb7e65afe5311407ecb903f65477592
Александр <yudenichaa@yandex.ru>
2020-04-09 15:22:05 +0300

    logo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import AdminPanel from './admin/AdminPanel';
 
-document.title = 'Технополис "ЭРА"';
+// document.title = 'Технополис "ЭРА"';
+
+// let link = document.createElement('link');
+// link.type = 'image/x-icon';
+// link.rel = 'shortcut icon';
+// link.href = 'logo_era.ico';
+// document.head.appendChild(link);
 
 ReactDOM.render(
     <AdminPanel />,
